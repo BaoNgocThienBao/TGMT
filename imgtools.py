6104ccd13aeb98961e48aa2b4d2a51a287e1ad48
BaoNgocThienBao <132985380+BaoNgocThienBao@users.noreply.github.com>
2024-10-10 17:10:51 +0700

    Add files via upload

---
+++ imgtools.py
@@ -0,0 +1,43 @@
+import os
+import cv2
+from PIL import Image
+import numpy as np
+def load_image(image_path):
+    """
+        Đọc ảnh từ đường dẫn cho trước và trả về đối tượng ảnh
+        Tham số: image_path
+        Giá trị trả về: đối tượng hình ảnh
+    """
+    try:
+        img = Image.open(image_path)
+        return img
+    except Exception as e:
+        print("Lỗi khi đọc hình ảnh từ: ", image_path, " ", e)
+        return None
+
+def is_image_file(file_path):
+    """
+        return: True - nếu là ảnh
+                False - nếu khác ảnh
+    """
+    extensions =(".jpg", ".jpeg", ".png", ".gif", ".bmp")
+    return file_path.lower().endswith(extensions);
+
+
+def get_image_list(folder_path):
+    image_list = []
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        filenames = os.listdir(folder_path)
+        for filename in filenames:
+            file_path=os.path.join(folder_path, filename)
+            if  os.path.isfile(file_path) and is_image_file(file_path):
+                img = load_image(file_path)
+                image_list.append(img)
+    return image_list
+
+def display(title, img):
+    cv2.imshow(title, img)
+    # Chờ một khoảng thời gian
+    cv2.waitKey(0)
+    # Đóng window
+    cv2.destroyAllWindows()
